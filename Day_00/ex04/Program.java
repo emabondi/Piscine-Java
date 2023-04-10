ef5ff3d156a787b080bfe1d45857bacf5ea12d57
emabondi <emabond69@gmail.com>
2023-04-10 23:59:46 +0200

    funziona manca stampa

--- Day_00/ex04/Program.java
+++ Day_00/ex04/Program.java
@@ -15,24 +15,24 @@ public class Program {
 		int best[] = new int[10];
 		byte count = 0;
 		for (int x = 0; x < 65535; x++) {
-			//System.out.println(x);
-			if (letters[x] != 0 && letters[x] >= letters[best[count]]) {
+			if (letters[x] != 0 && letters[x] > letters[best[count]]) {
 				int tmp;
 				best[count] = x;
-				if (count <= 8)
+				if (count < 1) {
 					count++;
-				if (count < 2)
-					continue ;
-				byte i = (byte)(count - 2);
-				while (i >= 0 && letters[best[i]] <= letters[best[i + 1]]) {
+					continue ;}
+				byte i = (byte)(count - 1);
+				while (i >= 0 && letters[best[i]] < letters[best[i + 1]]) {
 					tmp = best[i];
 					best[i] = best[i + 1];
 					best[i + 1] = tmp;
 					i--;
 				}
+				if (count <= 8)
+					count++;
 			}
 		}
-		for (byte i = 0; i < 9; i++) {
+		for (byte i = 0; i < count +1; i++) {
 			char c = (char)best[i];
 			System.out.println("char: "+c+" count: "+letters[best[i]]);
 		}
